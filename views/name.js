ca91aecd269b32a7b4c2df882d2fd80d073d23d5
Daniel Dominic Longo <danieldominiclongo@gmail.com>
2018-02-24 16:20:06 -0800

    project cards working on

--- views/name.js
+++ views/name.js
@@ -7,7 +7,7 @@ class Name extends Component {
 			<ScrollableAnchor id={"name"}>
 			<div>
 				<div class="container">
-					<img class="fullscreen" src="https://images.unsplash.com/photo-1485801468070-3c1af624702e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=531533ce9d4d1b224f666c383a0447e7&auto=format&fit=crop&w=500&q=60"/>
+					<img class="fullscreen" src="https://images.unsplash.com/photo-1478250242432-9381e12b763b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a5433befe8eb75c614a7010c8a9883fd&auto=format&fit=crop&w=1368&q=80"/>
 					<div class="centered"> 
 						<h2 class="nameText"> Daniel Longo </h2>
 						<br/>
--- views/projects.js
+++ views/projects.js
@@ -2,16 +2,18 @@ import React, {Component} from 'react';
 import { Card, Col, Row } from 'antd';
 import ScrollableAnchor from "react-scrollable-anchor"
 import { configureAnchors } from "react-scrollable-anchor"
+import {Button} from 'antd';
+import {Modal} from 'antd';
 configureAnchors({offset: -45, scrollDuration: 500})
-//project = {str projectName, str info,str(image adress) background,int id(number for link)}
-const project1 = {projectName:"Test", info:"testing the cards", id:"0" };
+//project = {str projectName, str info,str(image adress) background,str url}
+const project1 = {projectName:"Project Name", info:"a breif description...", url:"#name",catagory:"catagory" };
 class Projects extends Component {
 	constrctor() {
 	}
 
 	state = {
-		projects : [project1, project1, project1],
-		cardsPerRow : 3,
+		projects : [project1, project1, project1,project1,project1,project1,project1],
+		numCardsShow : 10,
 	}
 
 	componentWillMount() {
@@ -23,38 +25,44 @@ class Projects extends Component {
 	}
 	renderProjectCards() {
 		let projectCards =  (
-			<Row type="flex" gutter={10}>
+			<Row style={{marginLeft:10}} type="flex" gutter={10}>
 				{this.state.projects.map((project, index) => {
 					let cur_projectName = project["projectName"]
-					let cur_info = project["info"]
-					let cur_id = project["id"];
-					if ((index + 1) > this.state.cardsPerRow) {
+					let cur_info = project["info"];
+					let cur_url = project["url"];
+					let cur_catagory = project["catagory"]
+					if ((index) > this.state.numCardsShow) { //will display 1 less that numCardsShow b/c indexing, but will add extra card at end
 						return;
 					}
 					return (
 						<div key={index}>
 					{/* for horizontal flip flip-container vertical*/}
-						<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+						<div class="flip-container horizontal" ontouchstart="this.classList.toggle('hover');">
 							<div class="flipper">
 								<div class="front">
 
-									<Card className="projectCard" key={index} title={cur_projectName}>
-										{"background image"}
+									<Card className="projectCard" key={index} title={cur_projectName}
+									extra={cur_catagory}
+									cover= {<img alt="example" className="projectCardImage" src="https://images.unsplash.com/photo-1505283884983-cf47b9e0f9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5c514001a3b6a6a18e6716dec11ad066&auto=format&fit=crop&w=1650&q=80" />}
+									>
 									</Card>
 
 								</div>
 								<div class="back">
-
+									<a href={cur_url}>
 									<Card className="projectCard" key={index}>
 										{cur_info}
 									</Card>
-
+									</a>
 								</div>
 							</div>
 						</div>
 						</div>
 					);
 				})}
+				<Card className="projectCard">
+					{"see all"}
+				</Card>
 			</Row>
 		)
 		return projectCards;
