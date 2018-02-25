44930252aa8ecae8c362b4e6eb0506979c339ac1
Daniel Dominic Longo <danieldominiclongo@gmail.com>
2018-02-25 00:16:56 -0800

    updates

--- views/about.js
+++ views/about.js
@@ -9,7 +9,10 @@ class About extends Component {
 			<ScrollableAnchor id={"about"}>
 				<div className="mainView">
 					<h2> About </h2>
-					<p class="indent"> Hello, <p/> my name is Daniel Longo...</p>
+					<p class="indent"> Hello, </p>
+					<p class="indent">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce egestas, nunc dictum maximus aliquet, ante magna ultricies nulla, non ultricies enim felis at turpis. Praesent congue molestie nunc, aliquam consectetur augue viverra a. Donec ultrices eu lectus vel laoreet. Phasellus ac mollis ex, eget bibendum augue. Pellentesque sem metus, lobortis vel ligula non, tempus ultricies urna. Pellentesque ut consectetur libero, vitae efficitur massa. Pellentesque a tempus enim. Fusce justo nibh, sodales id iaculis at, rhoncus ultrices lacus. Pellentesque nec nisl risus. Nulla facilisi. Morbi ex arcu, venenatis at felis vel, vestibulum mattis nibh. Nunc interdum suscipit dui sed faucibus. Suspendisse vel nunc quis elit facilisis eleifend eu sed justo. Quisque convallis, felis scelerisque vehicula fringilla, arcu mauris porttitor lorem, vitae tristique justo arcu a mi. Praesent non mi finibus, vulputate sem eu, ullamcorper dui. Integer ac imperdiet lacus, ut sollicitudin velit.
+					</p>
 				</div>
 			</ScrollableAnchor>
 
