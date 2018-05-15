5a4551baf633bf4757dc3ba713a538ab4bf1a3b7
Daniel Longo <DanielLongo@Daniels-MBP-3.hsd1.ca.comcast.net>
2018-05-14 20:21:49 -0700

    updates

--- views/footer.js
+++ views/footer.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import { Layout, Menu, Icon } from 'antd';
+import {Row} from 'antd';
 const { Header, Content, Footer, Sider } = Layout;
 class MyFooter extends Component {
 	render() {
 		return(
             <Footer style={{ textAlign: 'center' }}>
                 <p> Daniel Longo © 2017 Created by Daniel Longo </p>
-                <a href="https://github.com/DanielLongo"> <Icon style={{color:"black", fontSize:"50px"}} type="github" /> </a>
+                <Row gutter={10}> 
+                    <a href="https://github.com/DanielLongo"> <Icon style={{color:"black", fontSize:"50px"}} type="github" /> </a>
+                    <a href="https://www.linkedin.com/in/daniel-longo-a1805a163/"> <Icon style style={{color:"black", fontSize:"50px"}} type="linkedin" /></a>
+                </Row>
             </Footer>
 
 		)
