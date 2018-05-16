bae5b89f99b21f2020ae51450c215c142b87699d
Daniel Longo <DanielLongo@Daniels-MBP-3.hsd1.ca.comcast.net>
2018-05-15 17:49:53 -0700

    updates

--- views/projects.js
+++ views/projects.js
@@ -6,6 +6,7 @@ import {Button} from 'antd';
 import {Modal} from 'antd';
 import AllProjects from "./allProjects"
 import HtmlComponent from "../components/htmlComponent"
+import { message} from 'antd';
 // import test from "../pages/projects/testProject.txt"
 
 
@@ -13,31 +14,31 @@ configureAnchors({offset: -45, scrollDuration: 500})
 //project = {str projectName, str info,str(image adress) background,str url}
 const img = "https://images.unsplash.com/photo-1505283884983-cf47b9e0f9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5c514001a3b6a6a18e6716dec11ad066&auto=format&fit=crop&w=1650&q=80"
 const project1 = {projectName:"Logistic Regression from Scratch", 
-info:"Logistic regression using only numpy to identify cows.", url:"#name",catagory:"ML" ,img:
+info:"Logistic regression using only numpy to identify cows.", url:"projects/Logistic_Regression_Model-notes.html",catagory:"ML" ,img:
 "https://images.unsplash.com/photo-1502590464431-3b66d77494d7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=464528617b90a6f30c1cbcd6db84d90b&auto=format&fit=crop&w=667&q=80"};
 
 const project2 = {projectName:"Neural Net from Scratch", 
-info:"A vanilla nerual net built entirely form scratch", url:"#name",catagory:"Deep Learning",
+info:"A vanilla nerual net built entirely form scratch", url:"projects/NN.html",catagory:"Deep Learning",
 img:img };
 
 const project3 = {projectName:"CNN from Scratch", 
-info:"A CNN built from numpy that classifies the type of leaf", url:"#name",catagory:"Deep Learning",
+info:"A CNN built from numpy that classifies the type of leaf", url:"#projects",catagory:"Deep Learning",
 img:"https://images.unsplash.com/photo-1459478309853-2c33a60058e7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ba9893452f120b4af8ffe3b3f9475e7f&auto=format&fit=crop&w=750&q=80" };
 
 const project4 = {projectName:"RNN Cryptocurrencey Predictor", 
-info:"A RNN built using tensorflow that relates cryptocurrencey and predicts how they will move in the future", url:"#name",catagory:"Deep Learning",
+info:"A RNN built using tensorflow that relates cryptocurrencey and predicts how they will move in the future", url:"#projects",catagory:"Deep Learning",
 img:"https://images.unsplash.com/photo-1519995451813-39e29e054914?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5c0478ad15d7b018f8bb59f644f17184&auto=format&fit=crop&w=750&q=80" };
 
 const project5 = {projectName:"Lidar Object Detection", 
-info:"A CNN built using keras that identifies obsticles in a field using lidar data", url:"#name",catagory:"Deep Learning",
+info:"A CNN built using keras that identifies obsticles in a field using lidar data", url:"#projects",catagory:"Deep Learning",
 img:"http://scanse.io/sites/default/files/product/image/FRONT.JPG" };
 
 const project6 = {projectName:"Image Collector", 
-info:"A simple scraper that downloads images using json data from imagenet. Used to accumulate image data for other projects", url:"#name",catagory:"Web Scraping",
+info:"A simple scraper that downloads images using json data from imagenet. Used to accumulate image data for other projects", url:"#projects",catagory:"Web Scraping",
 img:"https://patrykchrabaszcz.github.io/assets/img/Imagenet32/64x64.png" };
 
 const project7 = {projectName:"Ticket Price Analysis", 
-info:"Not quite an avid football fan I often sell my tickets season tickets on StubHub. In this project I collect ticket sales data and found when it is best to sell my ticket.", url:"#name",catagory:"Data Science",
+info:"Not quite an avid football fan I often sell my tickets season tickets on StubHub. In this project I collect ticket sales data and found when it is best to sell my ticket.", url:"#projects",catagory:"Data Science",
 img:"https://images.unsplash.com/photo-1508863702330-2eece3ea08cb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1d89a6172238b6b42f6b9cd47a37c976&auto=format&fit=crop&w=500&q=60" };
 
 const project8 = {projectName:"Simple WebGL Game in Unity",
@@ -46,7 +47,7 @@ url: "http://slidergame.s3-website-us-west-1.amazonaws.com/",
 img: "https://udemy-images.udemy.com/course/750x422/1210008_6859.jpg",
 catagory:"Other"}
 
-const project9 = {projectName:"Project Name", info:"a breif description...", url:"#name",catagory:"catagory",img:img };
+const project9 = {projectName:"Project Name", info:"a breif description...", url:"#projects",catagory:"catagory",img:img };
 
 
 // var html = "<p> This Works? </p>"
@@ -76,6 +77,11 @@ class Projects extends Component {
 		this.allProjectsModal = ""
 		console.log("cards here b:")
 		console.log(this.projectCards)
+		message.config({
+			top: 15,
+			duration: .75,
+			maxCount: 4,
+		  });
 		// console.log(test)
 	}
 
@@ -106,6 +112,16 @@ class Projects extends Component {
 	// 		}) 
 	// }
 
+	checkIfAvaliable(cur_url) {
+		if (cur_url == "#projects") {
+			console.log("LOGGED");
+			message.error("This project is not avaliable to view yet");
+		}
+		else {
+			console.log("Routing to " + cur_url);
+		}
+	}
+
 	renderProjectCards() {
 		let projectCards =  (
 			<Row style={{marginLeft:"1%"}} type="flex" gutter={10}>
@@ -132,7 +148,7 @@ class Projects extends Component {
 									</Card>
 
 								</div>
-								<div class="back">
+								<div onClick={() => this.checkIfAvaliable(cur_url)} class="back">
 									<a href={cur_url}>
 									<Card className="projectCard" key={index}>
 										<p className="projectCardDescription"> {cur_info} </p>
