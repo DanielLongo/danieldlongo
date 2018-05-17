3a9a120d456a72e97ed329130955381d36d83f2c
Daniel Longo <DanielLongo@Daniels-MBP-3.hsd1.ca.comcast.net>
2018-05-16 22:44:02 -0700

    updates

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import LogisticRegression from './pages/projects/logisticRegression'
 import Home from "./pages/home"
 import { removeHash } from 'react-scrollable-anchor'
 import { hotjar } from 'react-hotjar';
+import ReactGA from 'react-ga';
 // import "./views/canvaBackground.css"
 // import "./views/canvaBackground.html"
 
@@ -13,7 +14,9 @@ import { hotjar } from 'react-hotjar';
 
 class App extends Component { 
   render() {
-  	hotjar.initialize(798187, 6);
+    hotjar.initialize(798187, 6);
+    ReactGA.initialize('UA-115296983-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
     return (
     <div>
      	<Home/>
